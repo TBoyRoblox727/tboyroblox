598326e69b72055844596a0dd56612644dfe32f5
TBoyRoblox727 <trieutuyen802@gmail.com>
2024-12-15 08:18:55 +0700

    Add files via upload

---
+++ ScriptDemo.lua
@@ -0,0 +1,57 @@
+--Hạn Sử Dụng
+if os.date("%Y%m%d") >= "20230101" then
+gg.alert(" Script Hết Hạn","Ok")return end
+
+--Mật Khẩu
+Password = 'khoi2604'
+Pass = gg.prompt({"Nhập Pass Để Vào Script"},{},{"text"})
+if not Pass then os.exit() end
+if Pass[1] == Password then
+gg.toast('Đúng Là Con Trai Của Ta')
+else
+gg.alert('Xem Lại Video Đi Thằng Nhóc') os.exit()
+end
+
+function Main()
+Demo = gg.multiChoice({
+"Demo 1",--Chức Năng 1
+"Demo 2",--Chức Năng 2
+"Thoát"},nil,"Like và Đăng ký kênh TBoy Roblox Nha")
+if Demo == nil then
+else
+if Demo[1] == true then k1() end
+if Demo[2] == true then k2() end
+if Demo[3] == true then Exit() end
+end
+XGCK = -1
+end
+
+function k1()
+--Code Hack
+gg.alert("TBoy Roblox")
+end
+
+function k2()
+--Code Hack
+gg.alert("TBoy Roblox")
+end
+
+function Exit()
+gg.alert("😗Đăng Kí Kênh TBoy Roblox Nha🙏")
+gg.alert("🍌Like Và Đăng Kí Kênh Đê🖕")
+gg.alert("😈Xem Chùa Cắt Cu Giờ🤡")
+print("▒███████▒███▒▒▒██▒▒█▒▒▒█▒")
+print("▒▒▒▒█▒▒▒▒█▒▒█▒█▒▒█▒▒█▒█▒▒")
+print("▒▒▒▒█▒▒▒▒███▒▒█▒▒█▒▒▒█▒▒▒")
+print("▒▒▒▒█▒▒▒▒█▒▒█▒█▒▒█▒▒▒█▒▒▒")
+print("▒▒▒▒█▒▒▒▒███▒▒▒██▒▒▒▒█▒▒▒")
+os.exit()
+end
+while true do
+if gg.isVisible(true) then
+XGCK = 1
+gg.setVisible(false)
+end
+gg.clearResults()
+if XGCK == 1 then Main() end
+end
